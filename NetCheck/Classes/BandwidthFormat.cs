41ba2d840f6c75b61a7df97c4032a1a8e415165e
Amornthep Lertthasanawong <thep497@live.com>
2021-01-19 17:58:25 +0700

    Add project files.

---
+++ NetCheck/Classes/BandwidthFormat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iTuner
+{
+    public static class BandwidthFormat
+    {
+        public static string[] ordinals = new[] { "", "K", "M", "G", "T", "P", "E" };
+        public static string ToBWString(this long bandwidth_bps, bool is_XiB = false)
+        {
+            var _10x3 = is_XiB ? 1024 : 1000;
+            decimal rate = (decimal)bandwidth_bps;
+            var ordinal = 0;
+
+            while (rate >= _10x3)
+            {
+                rate /= _10x3;
+                ordinal++;
+            }
+
+            return (String.Format("{0:0.0} {1}bps",
+                    Math.Round(rate, 1, MidpointRounding.AwayFromZero),
+                    ordinals[ordinal]));
+        }
+    }
+}
---
+++ NetCheck/Main.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace NetCheck
+{
+	using iTuner;
+
+	public partial class Main : Form
+	{
+		public Main()
+		{
+			InitializeComponent();
+
+			NetworkStatus.AvailabilityChanged +=
+				new NetworkStatusChangedHandler(DoAvailabilityChanged);
+		}
+
+		private void Main_Load(object sender, EventArgs e)
+		{
+			ReportAvailability();
+		}
+		/// <summary>
+		/// Event handler used to capture availability changes.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+
+		void DoAvailabilityChanged(
+			object sender, NetworkStatusChangedArgs e)
+		{
+			ReportAvailability();
+		}
+
+		/// <summary>
+		/// Report the current network availability.
+		/// </summary>
+		private delegate void MyDelegate(string myText);
+		private void ReportAvailability()
+		{
+			if (NetworkStatus.IsAvailable)
+				label1.BeginInvoke(new MyDelegate(DelegateMethod),"On @ "+NetworkStatus.NetSpeed.ToBWString()); 
+			else
+				label1.BeginInvoke(new MyDelegate(DelegateMethod), "Off");
+		}
+
+		private void DelegateMethod(string myText)
+		{
+			label1.Text = myText;
+		}
+
+	}
+}
